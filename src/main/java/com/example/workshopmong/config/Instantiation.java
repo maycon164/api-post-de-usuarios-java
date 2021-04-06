de27cb41171416d27833297daffb76868e8f847d
Maycon <mayconfelipe164@gmail.com>
2021-04-06 13:56:37 -0300

    AuthorDTO para o Post

--- src/main/java/com/example/workshopmong/config/Instantiation.java
+++ src/main/java/com/example/workshopmong/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.workshopmong.domain.Post;
 import com.example.workshopmong.domain.User;
+import com.example.workshopmong.dto.AuthorDTO;
 import com.example.workshopmong.repository.PostRepository;
 import com.example.workshopmong.repository.UserRepository;
 
@@ -17,27 +18,29 @@ import com.example.workshopmong.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post p1 = new Post(null, sdf.parse("02/09/2018"), "Partiu São paulo", "Estou viajando amanhã para SP", bob);
-		Post p2 = new Post(null, sdf.parse("02/12/2020"), "Sangue de Jesus tem poder", "REAAU OU FEIKI", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post p1 = new Post(null, sdf.parse("02/09/2018"), "Partiu São paulo", "Estou viajando amanhã para SP",
+				new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("02/12/2020"), "Sangue de Jesus tem poder", "REAAU OU FEIKI",
+				new AuthorDTO(bob));
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
-		
+
 	}
 
 }
