a6e38c6ce6c88d0124f0b468d64e3debbe2962b6
Maycon <mayconfelipe164@gmail.com>
2021-04-06 13:42:05 -0300

    Post com User aninhado

---
+++ src/main/java/com/example/workshopmong/config/Instantiation.java
@@ -0,0 +1,43 @@
+package com.example.workshopmong.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.workshopmong.domain.Post;
+import com.example.workshopmong.domain.User;
+import com.example.workshopmong.repository.PostRepository;
+import com.example.workshopmong.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post p1 = new Post(null, sdf.parse("02/09/2018"), "Partiu São paulo", "Estou viajando amanhã para SP", bob);
+		Post p2 = new Post(null, sdf.parse("02/12/2020"), "Sangue de Jesus tem poder", "REAAU OU FEIKI", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(p1, p2));
+		
+	}
+
+}
---
+++ src/main/java/com/example/workshopmong/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.workshopmong.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.workshopmong.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
