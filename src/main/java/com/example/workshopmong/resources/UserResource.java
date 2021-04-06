b13ae6946c96aa510483d0f6b8bb66894d4b5601
Maycon <mayconfelipe164@gmail.com>
2021-04-06 09:49:12 -0300

    padrao DTO UserDTO

--- src/main/java/com/example/workshopmong/resources/UserResource.java
+++ src/main/java/com/example/workshopmong/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.workshopmong.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmong.domain.User;
+import com.example.workshopmong.dto.UserDTO;
 import com.example.workshopmong.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> lista = service.findAll();
-		return ResponseEntity.ok().body(lista);
+		List<UserDTO> listaDTO = lista.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);
 	}
 }
