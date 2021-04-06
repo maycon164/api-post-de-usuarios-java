fd85e4912aeb5e3ec01f1fc67c01191401c92b31
Maycon <mayconfelipe164@gmail.com>
2021-04-06 10:28:43 -0300

    findById e Exceptions ObjectNotFound

--- src/main/java/com/example/workshopmong/services/UserService.java
+++ src/main/java/com/example/workshopmong/services/UserService.java
@@ -1,19 +1,26 @@
 package com.example.workshopmong.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmong.domain.User;
 import com.example.workshopmong.repository.UserRepository;
+import com.example.workshopmong.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
 	UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id: " + id));
 	}
 }
---
+++ src/main/java/com/example/workshopmong/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.workshopmong.services.exception;
+
+public class ObjectNotFoundException  extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
