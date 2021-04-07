ce077412bff37560c3ed865df209edfaa3e2fa27
Maycon <mayconfelipe164@gmail.com>
2021-04-06 21:30:26 -0300

    post por id

---
+++ src/main/java/com/example/workshopmong/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.workshopmong.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmong.domain.Post;
+import com.example.workshopmong.repository.PostRepository;
+import com.example.workshopmong.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id: " + id));
+	}
+	
+}
